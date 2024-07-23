dbe25d8e0b4bc9d84514298f03adc1f85a9bf4c0
pyprince <wonderful.dru0923@outlook.com>
2024-07-23 09:13:59 +0300

    minor change, working on header and footer

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B7077",
+        secondary: "#696984"
+      }
+    },
+  },
+  plugins: [],
+}
