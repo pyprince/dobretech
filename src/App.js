fd6a96849ab93507a7c86173c7eedb35fe9e88f8
pyprince <wonderful.dru0923@outlook.com>
2024-07-24 19:16:13 +0300

    minor change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='max-w-[1400px] mx-auto bg-white'>
+    <div className='bg-white'>
       <Router>
         <Header />
         <Routes>
