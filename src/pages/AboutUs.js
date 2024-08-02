ac43d75f4fee0e70b772bb41b67fadc3f0186668
pyprince <wonderful.dru0923@outlook.com>
2024-08-02 23:41:32 +0300

    about us content change

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -59,26 +59,29 @@ const AboutUs = () => {
             <div className="dobre-signify text-center text-white px-5 py-16">
                 <h3 className="text-3xl sm:text-4xl font-semibold">What Does DobreTech Signify</h3>
                 <h4 className="text-2xl sm:text-3xl  leading-10 sm:px-20 mt-5 max-w-[1200px] mx-auto">Dobre is a Polish word meaning Good and that is where we got the name of our firm Dobre Technologies</h4>
-                <div className="py-2">
-                    <p className="text-xl py-3">Variants: Dobry, Dobriak</p>
+                {/* <div className="py-2"> */}
+                    {/* <p className="text-xl py-3">Variants: Dobry, Dobriak</p>
                     <p className="text-xl py-3">Etymology</p>
                     <p className="text-xl py-3">From dobrý (“good”)</p>
                     <p className="text-xl py-3">Pronunciation</p>
                     <p className="text-xl py-3">IPA(key): /ˈdɔbrɛ/</p>
                     <p className="text-xl py-3">Adverb</p>
-                    <p className="text-xl py-3">dobre (comparative lepšie, superlative najlepšie)</p>
-                </div>
+                    <p className="text-xl py-3">dobre (comparative lepšie, superlative najlepšie)</p> */}
+                {/* </div> */}
             </div>
             <div className="text-center mt-6 sm:mt-20 max-w-[1200px] mx-auto">
                 <h5 className="text-primary text-2xl sm:text-3xl font-semibold py-2">Welcome To DobreTech</h5>
                 <h4 className="text-primary text-3xl sm:text-5xl font-semibold py-4">New Experience Learn to the Next Level  Your Career</h4>
                 <p className="text-secondary text-xl leading-10 max-w-[800px] mx-auto">
-                    Architect client-centered total linkage for intuitive benefits. Dynamically restore convergence before real-time partnerships total linkage for intuitive benefits restore convergence before real-time.
+                    Were we turn every class into an engaging, practical and networking journey
+                    where our students learn theory, indepth concepts and perform hands-on practice in real-world scenarios.
+                    Looking for a change that brings more than just a job, but a better life? 
                 </p>
             </div>
             <div className="flex flex-wrap justify-center items-center gap-20 my-20">
                 <img src="/images/aboutus_career.png" alt="Career" className="w-[600px]" />
                 <div className="text-xl">
+                    <h6 className="text-2xl text-primary font-semibold py-5">DobreTech your number one Choice</h6>
                     <div className="flex items-center gap-3 text-secondary py-3">
                         <img src="/images/icons/dot.svg" alt="dot" />
                         <span>The foundation for a lifetime of success</span>
@@ -89,13 +92,13 @@ const AboutUs = () => {
                     </div>
                     <div className="flex items-center gap-3 text-secondary py-3">
                         <img src="/images/icons/dot.svg" alt="dot" />
-                        <span>Popular topics to learn now</span>
+                        <span>Popular topics in cutting-Edge Technology</span>
                     </div>
                     <div className="flex items-center gap-2 py-5">
                         <img src="/images/icons/user.svg" alt="user" className="w-20" />
                         <div>
-                            <h5 className="text-2xl text-primary">Mark Andersson</h5>
-                            <p className=" text-secondary">Project Manager</p>
+                            <h5 className="text-2xl text-primary">Kenne Japhet Elvis</h5>
+                            <p className=" text-secondary">Operations Director</p>
                         </div>
                     </div>
                     <Link to='./' className="bg-tomato text-white text-center text-base inline-block rounded-md w-40 py-2 active:font-semibold">Explore More</Link>
